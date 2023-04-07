097230185e09133fb9a621b2b65614fd4e79ca3b
MustaGitRefresh <kabcoco.2gitdom.3@gmail.com>
2023-04-07 16:07:28 +0530

    Learned escape sequence

---
+++ Escape_Sequence.java
@@ -0,0 +1,12 @@
+public class Escape_Sequence {
+    public static void main(String[] args) {
+        int a,b;
+        System.out.println("Different way of using backslash");
+        System.out.println("1 the use of backslash n along with the message");
+        System.out.println("Name: Akash Mittal \n Age: 16yrs \n Class X");
+        a = 14;
+        b = 16;
+        System.out.println("2 The use of backslash n separated by +");
+        System.out.println("First number = " + a + "\n" + "second number = " + b + "\n" + "Sum = " + (a+b));
+    }
+}
