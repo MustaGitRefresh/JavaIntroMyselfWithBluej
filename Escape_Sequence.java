f1697b1196d9e70ea26fb68dd1925560f4c9684b
MustaGitRefresh <kabcoco.2gitdom.3@gmail.com>
2023-04-07 16:22:01 +0530

    Learng something new in java that is final keyword which make the variable constant

--- Escape_Sequence.java
+++ Escape_Sequence.java
@@ -1,13 +1,21 @@
 public class Escape_Sequence {
     public static void main(String[] args) {
         int a,b;
+        final char c; // make it constant
         System.out.println("Different way of using backslash");
         System.out.println("1 the use of backslash n along with the message");
         System.out.println("Name: Akash Mittal \n Age: 16yrs \n Class X");
         a = 14;
         b = 16;
+        String max;
         System.out.println("2 The use of backslash n separated by +");
         System.out.println("First number = " + a + "\n" + "second number = " + b + "\n" + "Sum = " + (a+b));
         System.out.println('M');
+        max = "Gkljsdklfgjiojhbi;osdjgojhiosjgoijhiosjhoitjhojiojghoisrjgoijowtjhoijojhoijfoijgojerojghojrojoiajoirjhjtiortjoijh;oigjfpojojjogjowohjopjgsjfgp\\’kp[ihyu-=o054ikr9ug09wur0yiy-0if-0sf8g0-3i-0u6ij-]0nisawjret5yiuj0ruih0-i0ui0]-0tiapsj3iyukjposkrp’wuik0356-0ie0ru-00i5j409in0jqeujey09ui90h i30-iy0tiwi4w3oitfi7sdrkhbkjfjilioguiig9riohju89pu98p5uy89p4tg98rt76895098ut98rty09u09h90uter09hu9jeuh09utut09t094ui09hri09hygjbiosdjioyfiogbjdfoihnoijknotgkgogzkl;nksdjhiotjgioujiodhjgio;Jodi;jfo;jgoi;jgfiojio;hgjio;sjofijo;jhg;oiejiuogiobopjopfjroi;hjoisjtpoijoigfiogjtio;fjoi;fjhdoi;jho;dijhgi;ojdoi;njsdio;gjhiod;jgoi;hfiougjpojh;oibifjhgpoj;oisdjhgopsjuo;jgiofhio;jfiudhgoi;jhorigoiujrfsrjio;hgje";
+        System.out.println(max);
+        c = 'B';
+        System.out.println(c);
+        c = 'D';
+        System.out.println(c);
     }
 }
